b597483cb8438bd3828032e236a7983de9a7b0bd
David <125360594+HGSDavid@users.noreply.github.com>
2024-09-18 09:01:26 +0000

    Ampel gecodet

--- 01_Threads/Ampel/Ampel.java
+++ 01_Threads/Ampel/Ampel.java
@@ -1,5 +1,58 @@
 package Ampel;
 
-public class Ampel {
-    
+public class Ampel implements Runnable {
+
+    String ampelName;
+    private int wait;
+    Thread t;
+
+    // AmpelZustände
+    public static final int rot     = 1;
+    public static final int rotGelb = 2;
+    public static final int gelb    = 3;
+    public static final int grün    = 4;
+
+    private int Zustand = 1;
+
+    public Ampel(String ampelName, int speed) {
+        this.ampelName = ampelName;
+        this.wait = speed;
+        t = new Thread(this);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            switch (Zustand) {
+                case rot:
+                    System.out.println("Ampel rot");
+                    Zustand = rotGelb;
+                    break;
+
+                case rotGelb:
+                    System.out.println("Ampel Rot-Gelb");
+                    Zustand = grün;
+                    break;
+
+                case gelb:
+                    System.out.println("Ampel Gelb");
+                    Zustand = rot;
+                    break;
+
+                case grün:
+                    System.out.println("Ampel Grün");
+                    Zustand = gelb;
+            }
+
+            try {
+                Thread.sleep(wait);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void start() {
+        t.start();
+    }
 }
--- 01_Threads/Ampel/ErzeugeAmpel.java
+++ 01_Threads/Ampel/ErzeugeAmpel.java
@@ -1,5 +1,13 @@
 package Ampel;
 
 public class ErzeugeAmpel {
-    
+    public static void main(String[] args) {
+        System.out.println("----- Ampel gestartet -----");
+
+        //Threadobjekt erzeugen
+        Ampel a1 = new Ampel("Ampel1", 500);
+
+        //Thread nebenläufig starten
+        a1.start();
+    }
 }
